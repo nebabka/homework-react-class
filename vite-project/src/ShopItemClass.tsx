c9732d6f77264b9585e40f05cf239a15e2e449d8
nebabka <nastyfedinadura@gmail.com>
2025-09-19 16:06:14 +0300

    задания сделаны

---
+++ vite-project/src/ShopItemClass.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export interface Item {
+    brand: string,
+    title: string,
+    description: string,
+    descriptionFull: string,
+    price: number,
+    currency: string
+}
+
+export interface ShopItemClassProps {
+    item: Item
+}
+
+class ShopItemClass extends React.Component<ShopItemClassProps> {
+    render() {
+        const { item } = this.props;
+
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">{item.descriptionFull}</div>
+                <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{item.currency}{item.price}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ShopItemClass;
