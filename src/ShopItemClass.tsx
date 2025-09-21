45e96d2fac9a6831b2e8f144453f779d6e52c432
nebabka <nastyfedinadura@gmail.com>
2025-09-21 23:06:10 +0300

    задания сделаны

--- src/ShopItemClass.tsx
+++ src/ShopItemClass.tsx
@@ -23,7 +23,9 @@ class ShopItemClass extends React.Component<ShopItemClassProps> {
                 <h1>{item.title}</h1>
                 <h3>{item.description}</h3>
                 <div className="description">{item.descriptionFull}</div>
-                <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
                 <div className="divider"></div>
                 <div className="purchase-info">
                     <div className="price">{item.currency}{item.price}</div>
